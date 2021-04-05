5933b189e7520615ae022b43e405423b6fb2ea6d
Danny Hui <39434445+dhui808@users.noreply.github.com>
2021-04-04 20:22:56 -0400

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,3 +1,23 @@
+my.ini file in mysql home:
+  [mysqld]
+  # set basedir to your installation path
+  basedir=C:/software/mysql-8.0.23-winx64
+  # set datadir to the location of your data directory
+  datadir=C:/mydata
+
+  [mysql]
+  local_infile=1
+##############################################################
+start mysql server
+  mysqld --local_infile=1
+log in to mysql server and use a specific schema (databse)
+  mysql -u root -p --local-infile financial_products
+  
+export database
+  mysqldump -u root -p financial_products > mysql.sql
+import mysql database
+  mysql -u root -p -h localhost financial_products < mysql.sql
+
 LOAD DATA LOCAL INFILE  
 '/usr/myData.csv'
 INTO TABLE investing_staging_oil_wti
